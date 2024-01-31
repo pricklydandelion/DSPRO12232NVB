83562af6b076ad75e5136160060263888383f8eb
Phoenix <fedboyko@google.com>
2024-01-31 23:15:29 +0300

    запущено

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('main.html')
+
+    if flask.request.method == 'POST':
+        with open('knn_model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
+        exp = float(flask.request.form['Потребление смолы, г/м2'])
+        y_pred = loaded_model.predict([[exp]])
+
+        return render_template("main.html", result=y_pred)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
